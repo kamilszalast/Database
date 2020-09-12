191a6680caa3467b8d4c65c7b9c344c68ccef51a
Kamil <kamilszalast0@gmail.com>
2020-09-12 19:42:44 +0200

    Another commit, ContactFactory almost ready, CsvStrategy and XmlStrategy need to be done

--- src/interfaces/AbstractCustomersParserStrategy.java
+++ src/interfaces/AbstractCustomersParserStrategy.java
@@ -1,5 +1,6 @@
 package interfaces;
 
+import models.Contact;
 import models.Customer;
 
 import java.util.List;
@@ -8,4 +9,8 @@ public interface AbstractCustomersParserStrategy {
     boolean getFileType(String path);
 
     List<Customer> parseCustomersFromFile(String path);
+
+    List<String> parseContactsFromFile(String path);
+
+    List<Contact> createContact(List<String> contacts);
 }
